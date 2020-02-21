6016ee8094bf88a80a2a8986c4f2cfe2421980be
wanghuihui <wanghuihui@exploring.cn>
2020-02-21 17:16:00 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 // For example, on Mac: sudo npm run / sudo yarn
 const devServerPort = 9527 // TODO: get this variable from setting.ts
 const mockServerPort = 9528 // TODO: get this variable from setting.ts
-const name = '行知探索' // TODO: get this variable from setting.ts
+const name = 'whh' // TODO: get this variable from setting.ts
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '././' : '/',
